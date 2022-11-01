94a61d38994bfa70712e20fe8bc9f122ffb78498
MeaslyFire0 <MeaslyFire0@gmail.com>
2022-11-01 17:51:15 -0500

    created small changes to facilitate group work

---
+++ src/Components/navbar.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import logo from '../static/img/rig.png';
+
+import Home from '../pages/Home';
+import Prediction from '../pages/Prediction';
+import About from '../pages/About';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const Navbar= () =>{
+  return (
+  <div>
+    
+    <div class="min-h-full">
+  <nav class="bg-slate-800" id='navbar'>
+    <div class="mx-auto w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <a href='#'>
+            <Link to="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                A
+            </Link>
+
+            </a>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-5 flex items-baseline space-x-4">
+              {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+              
+                <Link to="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  B
+                  </Link>
+
+                <Link to="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                C
+              </Link>
+
+
+            
+            </div>
+          </div>
+        </div>
+       
+        <div class="-mr-2 flex md:hidden">
+          {/* <!-- Mobile menu button --> */}
+          <button type="button" class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            {/* <!--
+              Heroicon name: outline/bars-3
+
+              Menu open: "hidden", Menu closed: "block"
+            --> */}
+            <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            {/* <!--
+              Heroicon name: outline/x-mark
+
+              Menu open: "block", Menu closed: "hidden"
+            --> */}
+            <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+    <div class="md:hidden" id="mobile-menu">
+      <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+              {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+              
+                <Link to="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+
+                  </Link>
+                
+      </div>
+     
+    </div>
+  </nav>
+
+  <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+
+  
+</div>
+
+
+  </div>
+
+  );
+}
+export default Navbar;
+
+
+
