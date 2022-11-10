b58563ee55bd9248f2c4248dc82c43c0066a3934
MeaslyFire0 <MeaslyFire0@gmail.com>
2022-11-10 14:37:14 -0600

    trying to work routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
 import {
-  BrowserRouter
+  Routes,
+  Route
 } from "react-router-dom";
 
 import Login from './Pages/Login';
+import Home from './Pages/Home';
+import SignUp from './Components/SignUp';
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Login/>
-      </BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Home" element={<Home />} />
+      </Routes>
+        
+     
     </div>
   );
 }
