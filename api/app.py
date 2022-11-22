ac291ee8fe59fe7bff6f3b8e366a21c8565d0ed3
MeaslyFire0 <MeaslyFire0@gmail.com>
2022-11-22 00:04:57 -0600

    chanegd stuff in app.py, app.js and Login

--- api/app.py
+++ api/app.py
@@ -7,7 +7,9 @@ import sql_queries
 import json_responses as jsn_resp
 import datetime
 import preprocessing
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 #__functions__________________________________________________________________________________
 
